6b59d7b5897535acb29f3b7f9ec1f8827ac19607
Junbeom Chun <92chuck@gmail.com>
2022-07-09 14:44:47 -0400

    new web version with server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 7777;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
+
+io.on('connection', (socket) => {
+  console.log('A user just connected.');
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.');
+  });
+});
